f0772521014b8ff06c6caa1046da7317c5e35102
zamFe <zamfel18@student.kristiania.no>
2019-05-24 14:14:07 +0200

    updated user-management files

--- user management/js/users.js
+++ user management/js/users.js
@@ -1,52 +1,53 @@
 //global variables
 
-var user_id_count = 0;
-var users = {};
+var userIdCount = 0;
+var users = [{}]; //index 0 of filler
+var currentUser = 0; //0 = no user
 
-//user system
-
-/*
-var user = {
-    //essential
-    id: null,
-    userName: "",
-    password: "",
-    email: "";
-    //general
-    firstName: "",
-    lastName: "",
-    age: null,
-    //personalization
-    exp: 0,
-    badgeId: "",
-    themeId: "",
-    //storage
-    projects: []
-};
-'*/
+//methods
 
 //create new user with incremented id
 function createNewUser(userName, password, email, age, firstName, lastName) {
-    user_id_count++;
-    users['user_' + user_id_count] = {};
-    users['user_' + user_id_count].id = user_id_count;
-    users['user_' + user_id_count].userName = userName;
-    users['user_' + user_id_count].password = password;
-    users['user_' + user_id_count].password = email;
-    users['user_' + user_id_count].password = age;
-    users['user_' + user_id_count].firstName = firstName;
-    users['user_' + user_id_count].lastName = lastName;
-    users['user_' + user_id_count].exp = 0;
-    users['user_' + user_id_count].badgeId = "default";
-    users['user_' + user_id_count].themeId = "default";
-    users['user_' + user_id_count].projects = ["get_started"];
+    userIdCount++;
+    users[userIdCount] = {};
+    users[userIdCount].id = userIdCount;
+    users[userIdCount].userName = userName;
+    users[userIdCount].password = password;
+    users[userIdCount].email = email;
+    users[userIdCount].age = age;
+    users[userIdCount].firstName = firstName;
+    users[userIdCount].lastName = lastName;
+    users[userIdCount].exp = 0;
+    users[userIdCount].badgeId = "default";
+    users[userIdCount].themeId = "default";
+    users[userIdCount].projects = ["get_started"];
 }
 
+//returns user object from id
 function getUserData(id) {
     return users['user_' + id];
 }
 
+//finds corresponding user and set as logged in
+function login(username, password) {
+    console.log(users.length);
+    for (let i = 1; i < users.length; i++) {
+        if (users[i].userName.localeCompare(username) === 0 && users[i].password.localeCompare(password) === 0) {
+            currentUser = 1;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//DOM-methods
+function updateUserDiv(user) {
+
+}
+
+
 //TESTING
+
 createNewUser("Ola123", "passord123",
     "ola123@gmail.com", 22, "Ola",
     "Normann");
@@ -54,8 +55,5 @@ createNewUser("Arne420", "bengt123",
     "arne420@gmail.com", 17, "Arne",
     "420");
 
-function getUserData(userName, passWord) {
-    users.indexOf()
-}
-
 console.log(getUserData(2));
+console.log(login("Ola123", "passord123"));
