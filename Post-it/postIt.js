5c1daa8a79d354bf51f22e7dc027230ac03f5b0f
Kristian <kristian.garberg@gmail.com>
2019-05-27 16:33:59 +0200

    Post it note, updated.

--- Post-it/postIt.js
+++ Post-it/postIt.js
@@ -1,6 +1,7 @@
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
+
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
@@ -21,6 +22,7 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
+
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
@@ -29,10 +31,13 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
+
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
+
     li.appendChild(t);
+
     if (inputValue === '') {
         alert("You must write something!");
     } else {
@@ -42,6 +47,7 @@ function newElement() {
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
+
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
@@ -52,4 +58,6 @@ function newElement() {
             div.style.display = "none";
         }
     }
+
+
 }
