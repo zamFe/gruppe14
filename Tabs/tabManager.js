bc6bfc0aedbcc880385e918e7863de3bfe7fd6fc
zamFe <zamfel18@student.kristiania.no>
2019-05-28 14:22:22 +0200

    updated tab system

--- Tabs/tabManager.js
+++ Tabs/tabManager.js
@@ -1,11 +1,35 @@
 
 //variables
 
-
+var colorArr = ["White" + "Blue" + "Burgunder"];
 
 //methods
 
-function createNewtab() {
+function createNewTab(image) {
+
+    //get main container
+    var tabContainer = document.getElementById("tabContainer");
+
+    //create necessary elements
+    var tab = document.createElement("div");
+    var tabImg = document.createElement("img");
+
+    //add content
+    tabImg.src = image;
+
+    //add id and class
+    tab.classList.add("tab");
+    tabImg.classList.add("tabImg");
+
+    //put it on the website
+    tab.appendChild(tabImg);
+    tabContainer.appendChild(tab);
+
+    //update flex-grid order
+    var add = document.getElementById("add");
+    tab.style.order = add.style.order;
+    add.style.order++;
+
 
 }
 
