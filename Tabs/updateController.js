40bb18eb9a2ed0eaf91ee90703a6317b7eaf1656
zamFe <zamfel18@student.kristiania.no>
2019-06-06 14:51:26 +0200

    added saving and loading via JSON

--- Tabs/updateController.js
+++ Tabs/updateController.js
@@ -16,6 +16,10 @@ function saveProjects(projects) {
     localStorage.setItem("projects",JSON.stringify(projects));
 }
 
+function saveCurrentUser(currentUser) {
+    localStorage.setItem("currentUser",JSON.stringify(currentUser));
+}
+
 function saveUsersAndProjects(users, projects) {
     localStorage.setItem("users", JSON.stringify(users));
     localStorage.setItem("projects",JSON.stringify(projects));
@@ -29,4 +33,20 @@ function loadProjects() {
     return JSON.parse(localStorage.getItem("projects"));
 }
 
+function loadCurrentUser() {
+    return JSON.parse(localStorage.getItem("currentUser"));
+}
+
 //linear code
+var currentUser = 0;
+var users = [{}];
+var projects = [{}];
+if(users.length < loadUsers().length) {
+    users = loadUsers();
+}
+if(projects.length < loadProjects().length) {
+    projects = loadProjects();
+}
+if(currentUser < loadCurrentUser()) {
+    currentUser = loadCurrentUser();
+}
